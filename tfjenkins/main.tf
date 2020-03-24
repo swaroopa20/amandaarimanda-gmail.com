c931f166d5d88918e75867d35e6dbf7739a1ced7
srimul <prasad.mul@gmail.com>
2020-03-23 21:34:24 -0500

    jenkins installation

---
+++ tfjenkins/main.tf
@@ -0,0 +1,78 @@
+provider "aws" {
+   region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "clisrimul"
+    key    = "Jenkins/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+resource "aws_security_group" "allow_ssh" {
+  name = "sg_jenkins"
+  description = "Allow ssh inbound traffic"
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+      from_port = 8080
+      to_port = 8080
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "cZServers" {
+  ami = "${lookup(var.amiid, var.region)}"
+  instance_type = "t2.micro"
+  count = "${var.instance_count}"
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+  tags = {
+    Name = "Jenkins-${count.index + 1}"
+  }
+
+  key_name = "${var.key_name}"
+  
+#  provisioner "file" {
+#    source      = "/home/ubuntu/.ssh/id_rsa.pub"
+#    destination = "/tmp/id_rsa.pub"
+#  }
+
+  user_data = "${file("user-data.txt")}"
+
+connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    password = ""
+    private_key = "${file("~/srimul.pem")}"
+  }
+
+}
+
+output "public_ip" {
+#value = "${aws_instance.cZServers.public_ip}"
+  value = "${formatlist("%v", aws_instance.cZServers.*.public_ip)}"
+}
+
+resource "null_resource" "myPublicIps" {
+count = "${var.instance_count}"
+provisioner "local-exec" {
+      command = "echo '${element(aws_instance.cZServers.*.public_ip, count.index)}' >> hosts1"
+}
+}
+
---
+++ tfjenkins/variable.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  default = "us-east-2"
+}
+variable "amiid" {
+  type = "map"
+  default = {
+    us-east-2 = "ami-0fc20dd1da406780b"
+    us-east-1 = "ami-0400a1104d5b9caa1"
+    us-west-1 = "ami-03ba3948f6c37a4b0"
+  }
+  description = "You may added more regions if you want"
+}
+
+variable "instance_count" {
+  default = "1"
+}
+variable "key_name" {
+  default = "srimul"
+  description = "the ssh key to be used for the EC2 instance"
+}
+
